db91ea435d1eb0b26a80757b9b5e9a300cee87f5
yuliya_litvinova <yuliyalitvinovawork@gmail.com>
2023-01-12 21:58:55 +0300

    Доп задание 5v

--- src/Main.java
+++ src/Main.java
@@ -70,7 +70,7 @@ public class Main {
          System.out.println();
          System.out.println();
 
-         System.out.println("Задача 5b");
+         System.out.println("Задача 5б");
          int ChampagnePrice = 2870;
          int ChampagneCount = 7;
          int DiscountChampagne = 50;
@@ -79,6 +79,19 @@ public class Main {
          double TotalSum_b = ((ChampagneCount*ChampagnePrice)*(DiscountChampagne/100f)) + (BreadCount* BreadPrice);
         System.out.printf( "Сумма покупки: "+ "%.1f",TotalSum_b);
         System.out.println();
+        System.out.println();
+
+        System.out.println("Задача 5в");
+        double ChampignonsPrice = 436.0;
+        double ChampignonsWeight = 2.5;
+        double PorciniPrice = 578.0;
+        double PorciniWeight = 3.75;
+        double PorciniDiscount = 5.0;
+
+        double PorciniSumDiscount = ( (PorciniPrice * PorciniWeight) /100) * (100 - PorciniDiscount);
+        double TotalSum_v = PorciniSumDiscount + (ChampignonsPrice*ChampignonsWeight);
+
+        System.out.println("Сумма покупки: "+ TotalSum_v);
         System.out.println();
 
     }
