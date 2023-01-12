e1b5240b543291755c91171be412737501844b65
yuliya_litvinova <yuliyalitvinovawork@gmail.com>
2023-01-12 18:15:21 +0300

    Доп задание 4

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,14 @@ public class Main {
     }
 
     public static void task1 () {
+        System.out.println();
         System.out.println("Задача 1");
         int a = 47;
         int b = 13;
         var d = a % b;
         int c = a / b;
         System.out.println("47/13 = " + c + " и " +d +" в остатке");
+        System.out.println();
 
     }
 
@@ -25,21 +27,30 @@ public class Main {
         int c = a%10;
         int d = b+c;
         System.out.println( " Сумма чисел двузначного числа " + a + " равна " + d );
+        System.out.println();
 
     }
     public static void task3 () {
         System.out.println("Задача 3");
        var a = 345;
        int b = a/100;
-       int с = a/10;
-       int d = с%10;
+       int c = a/10;
+       int d = c%10;
        int e = a % 10;
        int f = b+d+e;
         System.out.println( " Сумма чисел трехзначного числа " + a + " равна " + f );
+        System.out.println();
     }
     public static void task4 () {
         System.out.println("Задача 4");
-        // Пишем код для задачи 2
+        int BouquetPrice = 4899;
+        int CardPrice = 157;
+        float TotalPrice = BouquetPrice + CardPrice;
+        float DiscountPrice = (TotalPrice / 10) *9 ;
+        System.out.println("Общая сумме покупки без скидки = " + TotalPrice);
+        System.out.println("Сумма покупки со скидкой  = " + DiscountPrice);
+        System.out.println( "Сумма скидки = " + (TotalPrice- DiscountPrice));
+        System.out.println();
     }
     public static void task5 () {
         System.out.println("Задача 5");
